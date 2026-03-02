d6c0d7fd979ce2f28ab4d109fe483249fbda6ce3
KuYart-PYPS <papot2012@gmail.com>
2026-03-02 20:40:25 +0700

    serve

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -640,6 +640,7 @@ export interface ApiMatchMatch extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    first_serve: Schema.Attribute.Enumeration<['A', 'B']>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::match.match'> &
       Schema.Attribute.Private;
