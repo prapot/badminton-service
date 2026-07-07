186a1c562283cdc308d9c55287c17a7eeb9ea379
KuYart-PYPS <papot2012@gmail.com>
2026-07-07 09:45:49 +0700

    update rank

--- src/api/ranking/services/ranking.ts
+++ src/api/ranking/services/ranking.ts
@@ -282,10 +282,10 @@ export default factories.createCoreService('api::ranking.ranking', ({ strapi }) 
     getRankInfoFromPoints(points: number) {
         const TIERS = [
             { name: 'Bronze', divisions: 3, starsPerDiv: 3 },
-            { name: 'Silver', divisions: 3, starsPerDiv: 4 },
-            { name: 'Gold', divisions: 4, starsPerDiv: 4 },
-            { name: 'Platinum', divisions: 5, starsPerDiv: 5 },
-            { name: 'Diamond', divisions: 5, starsPerDiv: 5 },
+            { name: 'Silver', divisions: 3, starsPerDiv: 3 },
+            { name: 'Gold', divisions: 4, starsPerDiv: 3 },
+            { name: 'Platinum', divisions: 5, starsPerDiv: 4 },
+            { name: 'Diamond', divisions: 5, starsPerDiv: 4 },
             { name: 'Master', divisions: 1, starsPerDiv: 99999 }
         ];
         const DIVS = ['V', 'IV', 'III', 'II', 'I'];
