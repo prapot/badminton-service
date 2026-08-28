be1c3d3fbab910d30871a62264bef6b8cccb0227
KuYart-PYPS <papot2012@gmail.com>
2026-08-29 00:30:31 +0700

    new season

--- src/api/ranking/services/ranking.ts
+++ src/api/ranking/services/ranking.ts
@@ -286,9 +286,9 @@ export default factories.createCoreService('api::ranking.ranking', ({ strapi }) 
         const TIERS = [
             { name: 'Bronze', divisions: 3, starsPerDiv: 3 },
             { name: 'Silver', divisions: 3, starsPerDiv: 3 },
-            { name: 'Gold', divisions: 4, starsPerDiv: 3 },
-            { name: 'Platinum', divisions: 4, starsPerDiv: 4 },
-            { name: 'Diamond', divisions: 4, starsPerDiv: 4 },
+            { name: 'Gold', divisions: 3, starsPerDiv: 3 },
+            { name: 'Platinum', divisions: 3, starsPerDiv: 3 },
+            { name: 'Diamond', divisions: 3, starsPerDiv: 3 },
             { name: 'Master', divisions: 1, starsPerDiv: 99999 }
         ];
         const DIVS = ['V', 'IV', 'III', 'II', 'I'];
